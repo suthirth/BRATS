f4a99ad396d4912d4306e2192d835aa5cccceee8
suthirth <suthirthv@gmail.com>
2014-10-04 02:04:01 +0530

    EM GMM
    Debug left

--- BuildGMM.m
+++ BuildGMM.m
@@ -1,48 +1,79 @@
-function model = BuildGMM(trainData, truthData)
-%Labels:
-%Labeled from Training Data: 
-%1 - Necrosis 
-%2 - Surrounding Edema
-%3 - Non Enhancing Tumor
-%4 - Enhancing Tumor
-%Unsupervised Labeling
-%5 - White Matter
-%6 - Gray Matter
-%7 - CSF
+function model = BuildGMM(trainData)
+%%
+%Inputs:
+%trainData: col(1) Intensity Values col(2) Label (1/2/3/4/0/8)
+%Labels: 
+%1 - Necrosis 2 - Surrounding Edema 3 - Non Enhancing Tumor 4 - Enhancing Tumor
+%5 - White Matter 6 - Gray Matter 7 - CSF
+%8 - Non-Brain
+%Output:
+%model: (cols) weight, mean, std devn (rows) class 1,2,3..
 
-%Initialize, resize trainData and all that
-
-x = size(trainData,1);
-y = size(trainData,2);
-z = size(trainData,3);
-
-if (size(truthData) ~= size(trainData))
-    fprintf('Size mismatch');
+%% 
+%Initialize cluster centres for all classses
+%Use truth table to estimate intial for classes 1-4 
+for i=1:4
+    id = find(trainData(:,2) == i);
+    labelData = double(trainData(id,1));
+    CC_tumor(i,1) = mean(labelData);
+    CC_tumor(i,2) = cov(labelData);
+    CC_tumor(i,3) = size(id)/size(find(trainData(:,2) ~= 8));
 end
 
-classData = zeros(x*y*z,7);
-count = zeros(1,7);
-
-for i=1:x
-    for j=1:y
-        for k=1:z
-            if(truthData(x,y,z) ~= 0)
-                truthData(x,y,z) = 5; %Transfer all zero (unlabeled) to 5 (arbitrarily chosen)
-            end
-            count(truthData(x,y,z)) = count(truthData(x,y,z)) + 1;
-            classData(count(truthData(x,y,z)),truthData(x,y,z)) = trainData(x,y,z);
-        end
-    end
+%K-Means to estimate initial for classes 5-7 
+classData = double(trainData(find(trainData(:,2) == 0),1));
+[IDX,CC_normal] = kmeans(classData, 3, 'Start', 'uniform', 'Display','iter'); 
+for i=1:3
+    id = find(IDX(:,1) == i);
+    labelData = double(classData(id));
+    CC_normal(i,2) = cov(labelData);
+    CC_normal(i,3) = size(id)/size(find(trainData(:,2) ~= 8));
 end
+
+model = [CC_normal; CC_tumor];
 
 %%
-%K-Means to calculate cluster centres before GMM
+%Expectation Maximisation to calculate final GMM parameters
 
+%Initialization
+brainData = double(trainData(find(trainData(:,2)~=8),1));
+gamma = zeros(size(brainData),7);
+for i=1:size(brainData,1)
+    for j=1:7
+        J(i,j) = model(j,3) * ((2*pi)^(-0.5)) * (model(j,2)^-0.5) * exp(-((brainData(i)-model(j,1))^2)/(2*model(j,2)));
+    end
+end
+j_new = sum(log(sum(J,2)),1);
+j_old = j_new+1;
 
+%EM iterations
+while j_new < j_old
+    j_old = j_new;
+    %E Step
+    for i=1:size(brainData,1)
+        for j=1:7
+            gamma(i,j) = model(j,3) * ((2*pi)^(-0.5)) * (model(j,2)^-0.5) * exp(-((brainData(i)-model(j,1))^2)/(2*model(j,2)));
+        end
+        gamma(i,:) = gamma(i,:)./sum(gamma(i,:));
+    end
+    
+    %M Step
+    for j =1:7
+            model(j,1) = sum(gamma(:,j).*brainData)/sum(gamma(:,j));
+            model(j,2) = sum(gamma(:,j).*(brainData - model(j,1).^2))/sum(gamma(:,j));
+            model(j,3) = sum(gamma(:,j))/size(brainData,1);
+    end
+    
+    %Log Likelihood
+    for i=1:size(brainData,1)
+        for j=1:7
+            J(i,j) = model(j,3) * ((2*pi)^(-0.5)) * (model(j,2)^-0.5) * exp(-((brainData(i)-model(j,1))^2)/(2*model(j,2)));
+        end
+    end
+    j_new = sum(log(sum(J,2)),1)
+end
 
-
-
-
+end
                         
 
 
--- runGMM.m
+++ runGMM.m
@@ -1,7 +1,37 @@
-info = mha_read_header('BRATS_HG0002_T1C.mha');
-X = mha_read_volume(info);
-imshow(squeeze(X(:,:,88)),[]);
+%%
+%Read all Training Data. eg- T1C HG files
+trainData = [];
+for i=1:5%7
+    filename = sprintf('BRATS_HG%.4d_T1.mha',i);
+    info = mha_read_header(filename);
+    intensityValues = mha_read_volume(info);
+    truthFile = sprintf('BRATS_HG%.4d_truth.mha',i);
+    info = mha_read_header(truthFile);
+    truthValues = mha_read_volume(info);
+    dim = size(intensityValues);
+    trainData = [trainData; reshape(intensityValues,dim(1)*dim(2)*dim(3),1) reshape(truthValues,dim(1)*dim(2)*dim(3),1)];
+end
+
+%%
+%Label NAs as 8 (Non-Brain)
+x = size(trainData,1);
+for i=1:x
+    if(trainData(i,1) == 0)
+        trainData(i,2) = 8;
+    end
+end
+
+%Display Histogram
+%classData = trainData(find(trainData(:,2) == 0),1);
+%hist(double(classData),50);
+
+%Build GMM
+model = BuildGMM(trainData);
+       
+%plot(squeeze(X(:,108,88)),[]);
 
 %Prepare Mask
 
-model = BuildGMM(trainData, truthData);
+
+
+%model = BuildGMM(trainData, truthData);
