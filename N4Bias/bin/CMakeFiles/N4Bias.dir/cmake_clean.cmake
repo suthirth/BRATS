c493dad4b81fcad0f3aeb02f32b41771939fd90c
suthirth <suthirthv@gmail.com>
2015-01-16 12:19:25 +0530

    Added N4ITK Bias Field Correction. Working fine.

---
+++ N4Bias/bin/CMakeFiles/N4Bias.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/N4Bias.dir/N4Bias.cxx.o"
+  "N4Bias.pdb"
+  "N4Bias"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/N4Bias.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
