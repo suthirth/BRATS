b1ac817514601b34ec17850aa90e1381183a3178
suthirth <suthirthv@gmail.com>
2015-01-19 21:07:36 +0530

    Changed name of executable

---
+++ ExtractFeatures/bin/CMakeFiles/ExtractFeatures.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/ExtractFeatures.dir/ExtractFeatures.cxx.o"
+  "ExtractFeatures.pdb"
+  "ExtractFeatures"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/ExtractFeatures.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
