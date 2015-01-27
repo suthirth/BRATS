4b65edfaf05c09c6081f68183a05f7e7dc0c8eac
suthirth <suthirthv@gmail.com>
2015-01-27 23:08:02 +0530

    minor fixes

--- features.py
+++ features.py
@@ -3,13 +3,13 @@ import os
 BRATSLIB_PATH = '/home/brats/BRATS/'
 
 #Read files 
-trpath = '/Dataset/'
+trpath = '/Test/'
 trpath, patients, files = os.walk(trpath).next()
 
 for p in patients:
-    if not os.path.exists(trpath+p+'/Features'):
-        os.makedirs(trpath+p+'/Features')
     imgs = os.listdir(trpath+'/'+p)
+    if not os.path.exists(trpath+p+'/Features'):
+        os.makedirs(trpath+p+'/Features'
     for i in imgs:
         os.system(BRATSLIB_PATH+'ExtractFeatures/bin/ExtractFeatures '+trpath+p+'/ '+i+' '+trpath+p+'/Features/')
 
--- predictRF.py
+++ predictRF.py
@@ -6,8 +6,9 @@ import numpy as np
 
 image_type = itk.Image[itk.US,3]
 reader = itk.ImageFileReader[image_type].New()
+writer = itk.ImageFileWriter[image_type].New()
 itk_py_converter = itk.PyBuffer[image_type]
-path = '/Dataset/brats_tcia_pat105_1'
+path = '/Test/brats_tcia_pat138_1'
 
 seqindex = {'T1.':0,'T1C':1,'T2':2,'Flair':3}
 featureindex = {'gauss_3':1,'gauss_7':2,'kurt_1':3,'kurt_3':4,'max_1':5,'max_3':6,'mean_1':7,'mean_3':8,'min_1':9,'min_3':10,'skw_1':11,'skw_3':12,'std_1':13,'std_3':14} 
@@ -44,4 +45,8 @@ print 'Prediction in progress...'
 pred = rf.predict(features)
 x = pred.reshape(155,240,240)
 print 'Saving...'
-np.savetxt('rfoutput.txt',x[78])
+output_image = itk_py_converter.GetImageFromArray(x)
+writer.SetFileName('/RF/RFprediction.mha')
+writer.SetInput(output_image)
+writer.Update()
+#np.savetxt('rfoutput.txt',x[78])
