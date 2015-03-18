c999968405c31ba57ece4bb99b1ca84e2c5f55dd
suthirth <suthirthv@gmail.com>
2015-03-19 00:51:40 +0530

    Bounding box. All patches

--- 2D CNN/runCNN.py
+++ 2D CNN/runCNN.py
@@ -2,6 +2,8 @@ import os
 import time
 from time import gmtime, strftime
 
+import nibabel as nib
+
 import numpy as np
 import numpy
 import cPickle
@@ -21,13 +23,16 @@ import readPatMS
 #Training Parameters
 
 learning_rate=0.0001
-n_epochs=200
-nkerns=[20, 30, 50, 8] 
+n_epochs=30
+nkerns=[40, 50, 200, 8] 
 
 #####################
 #Load Datasets
 
 num_channels = 4
+x = (20,160)
+y = (17,192)
+z = (11,151)
 img_shape = [181,217,181]
 num_patients = 4
 TStamps = [4,4,5,4]
@@ -41,7 +46,7 @@ test_pat_num = 1
 test_Tstamp = 1
 
 pat = readPatMS.new(1,1)
-num_patches = np.shape(image.extract_patches_2d(pat.data[0,:,:,0],patch_size))[0]
+num_patches = np.shape(image.extract_patches_2d(pat.data[0,20:160,17:192,0],patch_size))[0]
 
 train_patches = np.zeros([num_patches, num_channels, patch_size[0], patch_size[1]])
 trpatches_truth = np.zeros([num_patches])
@@ -50,7 +55,6 @@ shared_data = theano.shared(numpy.asarray(train_patches,dtype = theano.config.fl
 shared_truth = theano.shared(numpy.asarray(trpatches_truth,dtype = 'int32'),borrow = True)
 
 rng = numpy.random.RandomState(23455)
-
 
 #Define Theano Tensors
 nz = T.lscalar()
@@ -93,9 +97,9 @@ layer3 = LogisticRegression(input=layer2.output, n_in=nkerns[2], n_out=nkerns[3]
 
 cost = layer3.negative_log_likelihood(y)
 
-test_model = theano.function([nz], layer3.y_pred, givens={ x: shared_data[nz*num_patches:(nz+1)*num_patches,:,:,:]})
+test_model = theano.function([], layer3.y_pred, givens={ x: shared_data})
 
-validate_model = theano.function([nz], layer3.errors(y), givens={x: shared_data[nz*num_patches:(nz+1)*num_patches,:,:,:], 
+validate_model = theano.function([nz], layer3.errors(y), givens={x: shared_data[:,nz*num_patches:(nz+1)*num_patches,:,:], 
                                                                 y: shared_truth[nz*num_patches:(nz+1)*num_patches]})
 
 params = layer3.params + layer2.params + layer1.params + layer0.params
@@ -104,30 +108,22 @@ grads = T.grad(cost, params)
 
 updates = [(param_i, param_i - learning_rate * grad_i) for param_i, grad_i in zip(params, grads)]
 
-train_model = theano.function([nz], cost, updates=updates, givens={ x: shared_data[nz*num_patches:(nz+1)*num_patches,:,:,:], 
+train_model = theano.function([nz], cost, updates=updates, givens={ x: shared_data[:,nz*num_patches:(nz+1)*num_patches,:,:], 
                                                                     y: shared_truth[nz*num_patches:(nz+1)*num_patches]})
 
 # TRAIN MODEL #
 ###############
 print '... training'
 
-patience = 100000  
-patience_increase = 2 
-improvement_threshold = 0.995 
-validation_frequency = 400000
-saving_frequency = 500000
+saving_frequency = 150
 
-best_validation_loss = numpy.inf
-best_iter = 0
-test_score = 0.
 start_time = time.clock()
 
 epoch = 0
-done_looping = False
 
 logcost = []
 
-while (epoch < n_epochs) and (not done_looping):
+while (epoch < n_epochs):
     epoch = epoch + 1
     iter = 0
     
@@ -135,31 +131,27 @@ while (epoch < n_epochs) and (not done_looping):
         
         for index in xrange(TStamps[pat_idx]):
                         
-            pat = readPatMS.new(pat_idx+1,index+1)
-
+            #pat = readPatMS.new(pat_idx+1,index+1)
             # Number of batches -> 10 slices
             # Each batch -> 32k patches from each slice
-            
-            z = 0
+            z = 11
 
-            slice_batch = 10
+            slice_batch = 35
 
-            while z < slice_batch*int(img_shape[2]/slice_batch):
+            while z < 151:
 
                 train_patches = []
-
                 print "... preparing patches"
 
                 for ch in xrange(num_channels):
-                    tr_patches = image.extract_patches_2d(pat.data[ch,:,:,z:z+slice_batch], patch_size))
-                    tr_patches = np.transpose(train_patches,[3,0,1,2])
-                    tr_patches = np.reshape(train_patches,[num_patches*slice_batch,patch_size[0],patch_size[1]])
-
-                train_patches.append(tr_patches)
-
-                truth_patches = image.extract_patches_2d(pat.truth[:,:,z:z+slice_batch], patch_size)
-                truth_patches = np.transpose(train_patches,[3,0,1,2])
-                truth_patches = np.reshape(train_patches,[num_patches*slice_batch,patch_size[0],patch_size[1]])                
+		    tr_patches = image.extract_patches_2d(pat.data[ch,20:160,17:192,z:z+slice_batch], patch_size)
+                    tr_patches = np.transpose(tr_patches,[3,0,1,2])
+                    tr_patches = np.reshape(tr_patches,[num_patches*slice_batch,patch_size[0],patch_size[1]])
+                    train_patches.append(tr_patches)
+		
+                truth_patches = image.extract_patches_2d(pat.truth[20:160,17:192,z:z+slice_batch], patch_size)
+                truth_patches = np.transpose(truth_patches,[3,0,1,2])
+                truth_patches = np.reshape(truth_patches,[num_patches*slice_batch,patch_size[0],patch_size[1]])                
                 
                 patches_truth = np.zeros([num_patches*slice_batch])
                 for i in xrange(num_patches*slice_batch):
@@ -174,59 +166,8 @@ while (epoch < n_epochs) and (not done_looping):
                 
                 z = z + slice_batch
                 logcost.append(cost_ij)
-
-                if (iter + 1) % validation_frequency == 0:
-                    
-                    print "... validation"
-                    
-                    validation_losses = []
-                    for vindex in xrange(valid_Tstamps):
-
-                        vpat = readPatMS.new(valid_pat_num,vindex+1)
-
-                        vz = 0
-
-                        while vz < int(img_shape[2]/10):
-               
-                            train_patches0 = image.extract_patches_2d(vpat.data[0,:,:,z:z+10], patch_size)
-                            train_patches1 = image.extract_patches_2d(vpat.data[1,:,:,z:z+10], patch_size)
-                            train_patches2 = image.extract_patches_2d(vpat.data[2,:,:,z:z+10], patch_size)
-                            train_patches = image.extract_patches_2d(vpat.data[3,:,:,z:z+10], patch_size)
-                            
-                            truth_patches = image.extract_patches_2d(pat.truth[:,:,z:z+10], patch_size)
-
-                            patches_truth = np.zeros([num_patches,10])
-                            
-                            for iz in range(10):
-                                for p in num_patches:
-                                    patches_truth[p,iz] = p[(patch_size[0]-1)/2, (patch_size[0]-1)/2,iz]
-
-                            z = z + 10
-
-                            shared_data.set_value(numpy.asarray([train_patches0,train_patches1,train_patches2,train_patches3],dtype = theano.config.floatX))
-                            shared_truth.set_value(numpy.asarray(patches_truth,dtype = 'int32'))
-
-                            validation_losses = validate_model()
-                            print np.shape(validation_losses)
-                        
-                    this_validation_loss = numpy.mean(validation_losses)
-
-                    print('Epoch %i, validation error %f %%' %
-                          (epoch, this_validation_loss * 100.))
-
-                    # if we got the best validation score until now
-                    if this_validation_loss < best_validation_loss:
-
-                        #improve patience if loss improvement is good enough
-                        if this_validation_loss < best_validation_loss *  \
-                           improvement_threshold:
-                            patience = max(patience, iter * patience_increase)
-
-                        # save best validation score and iteration number
-                        best_validation_loss = this_validation_loss
-                        best_iter = iter
-
-                if (iter +1) % saving_frequency == 0:
+                
+                if (iter+1) % saving_frequency == 0:
                     print 'Saving model...'
                     save_file = file('CNNmodel.pkl', 'wb')
                     for i in xrange(len(params)):
@@ -238,66 +179,29 @@ while (epoch < n_epochs) and (not done_looping):
                         myfile.write("%f\n"%l)
                     logcost = []
 
-                if patience <= iter:
-                    done_looping = True
-                    break
+                    pat = readPatMS.new(1,1)
+                    pat_data = numpy.asarray(pat.data,dtype = theano.config.floatX)
+
+                    Prediction = np.zeros([122,157,140])
+
+                    for z in xrange(11,151):
+                        print 'Predicting... slice:',(60+z)
+                        Pred = []
+                        tr_patches = np.asarray([image.extract_patches_2d(pat.data[ch,20:160,17:192,z], patch_size) for ch in xrange(num_channels)])
+                        shared_data.set_value(numpy.asarray(tr_patches[:,0:num_patches,:,:],dtype = theano.config.floatX))
+                        p1 = test_model() 
+                        shared_data.set_value(numpy.asarray(tr_patches[:,num_patches:num_patches*2,:,:],dtype = theano.config.floatX))
+                        p2 = test_model()
+                        Pred = np.append(p1,p2)
+                        Prediction[:,:,z] = np.reshape(np.asarray(Pred),[122,157])
+
+                    #output nii file
+                    affine = [[-1,0,0,0],[0,-1,0,0],[0,0,1,0],[0,0,0,1]]
+                    img = nib.Nifti1Image(Prediction, affine)
+                    img.set_data_dtype(numpy.int32)
+                    nib.save(img,'prediction.nii')
 
                 iter = iter + 1
 
-
 end_time = time.clock()
-print('Optimization complete.')
-print('Best validation score of %f %% obtained at iteration %i, '
-      'with test performance %f %%' %
-      (best_validation_loss * 100., best_iter + 1, test_score * 100.))
-print >> sys.stderr, ('The code for file ' +
-                      os.path.split(__file__)[1] +
-                      ' ran for %.2fm' % ((end_time - start_time) / 60.))
-
-
-# test it on the test set
-print "Prediction..."
-start_time = time.clock()
-
-pat = readPatMS.new(1,1)
-
-pat_data = numpy.asarray(pat.data,dtype = theano.config.floatX)
-pat_truth = numpy.asarray(pat.truth,dtype = 'int32')
-
-# One batch -> 80+ patches from slice where stride = patch_len
-Prediction = np.zeros(img_shape)
-num_batches = patch_size[0]*patch_size[1]
-num_patches = [int(img_shape[i]/patch_size[i])-1 for i in range(2)]
-
-for z in xrange(img_shape[2]):
-
-    test_patches = np.zeros([num_batches, num_patches[0]*num_patches[1], num_channels, patch_size[0], patch_size[1]])
-    test_pred = np.zeros([num_batches, num_patches[0]*num_patches[1]])
-                
-    i = 0 
-    for off_x in xrange(patch_size[0]):
-        for off_y in xrange(patch_size[1]):
-            test_patches[i,:,:,:,:] = [pat_data[:,
-                            off_x + ix*patch_size[0]: off_x + (ix+1)*patch_size[0],
-                            off_y + iy*patch_size[1]: off_y + (iy+1)*patch_size[1],
-                            z] for ix in range(num_patches[0]) for iy in range(num_patches[1])]
-            i = i+1
-
-    shared_data.set_value(numpy.asarray(test_patches,dtype = theano.config.floatX))
-
-    for off_x in xrange(patch_size[0]):
-        for off_y in xrange(patch_size[1]):
-            pred = test_model(off_x*off_y + off_y)
-            for ix in range(num_patches[0]):
-                for iy in range(num_patches[1]):
-                    Prediction[off_x + ix*patch_size[0] + (patch_size[0]-1)/2,
-                        off_y + iy*patch_size[1] + (patch_size[1]-1)/2,
-                        z] = pred[ix*iy + iy] 
-
-end_time = time.clock()
-print >> sys.stderr, ('Prediction done and it took '+' %.2fm ' % ((end_time - start_time) / 60.))
-#output nii file
-affine = [[-1,0,0,0],[0,-1,0,0],[0,0,1,0],[0,0,0,1]]
-img = nib.Nifti1Image(Prediction, affine)
-img.set_data_dtype(numpy.int32)
-nib.save(img,'prediction.nii')
+print 'Optimization complete. Time taken:', (end_time - start_time)/3600
